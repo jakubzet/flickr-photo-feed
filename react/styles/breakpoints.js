ba632ac8bb7dcd9fa035fbbcc4a0f49cccafc7f7
jakubzet <jakub.zywiec@gmail.com>
2019-12-25 11:16:15 +0100

    Updated CSS media query breakpoints

--- react/styles/breakpoints.js
+++ react/styles/breakpoints.js
@@ -3,9 +3,10 @@ import { rem } from "polished";
 const breakpoints = {
   base: 0,
   small: rem(319),
-  medium: rem(799),
-  large: rem(1199),
-  xlarge: rem(1919)
+  medium: rem(767),
+  large: rem(1023),
+  xlarge: rem(1279),
+  xxlarge: rem(1919)
 };
 
 export default breakpoints;
