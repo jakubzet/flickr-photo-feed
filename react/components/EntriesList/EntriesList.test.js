639b27000994e9c09cef52c42ac28519d13cef32
jakubzet <jakub.zywiec@gmail.com>
2019-12-27 01:22:20 +0100

    Added snapshot tests

--- react/components/EntriesList/EntriesList.test.js
+++ react/components/EntriesList/EntriesList.test.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "../../utils/reactTestingLibrary";
 import { EntriesList } from "./EntriesList";
 
 describe("EntriesList", () => {
   it("Renders", () => {
+    const sampleEntries = [
+      {
+        title: "Lorem ipsum",
+        link: "#",
+        media: {
+          m: "#"
+        },
+        date_taken: "Fri Dec 27 2019 01:14:14 GMT+0100",
+        description: "Lorem ipsum",
+        published: "Fri Dec 27 2019 01:14:14 GMT+0100",
+        author: "Lorem ipsum",
+        author_id: "id1",
+        tags: "Lorem ipsum"
+      }
+    ];
+
     const { container, asFragment } = render(
-      <EntriesList isFetching={false} />
+      <Router>
+        <EntriesList isFetching={false} entries={sampleEntries} />
+      </Router>
     );
 
     expect(container).toBeDefined();
